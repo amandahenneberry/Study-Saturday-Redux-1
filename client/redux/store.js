eab4384f4ae012461cfb362fd35b1a5fd74ccc13
Amanda Henneberry <amandahenneberry@icloud.com>
2021-12-04 15:43:19 -0500

    Update store.js

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,25 +1,59 @@
 import {createStore, applyMiddleware} from 'redux';
 import loggerMiddleware from 'redux-logger';
+import axios from 'axios';
+import thunkMiddleware from 'redux-thunk';
 
 // ACTION TYPES go here:
+const GOT_STUDENTS = 'GOT_STUDENTS';
+const GOT_SINGLE_STUDENT = 'GOT_SINGLE_STUDENT'
 
 
 // ACTION CREATORS go here:
+const gotStudents = (students) => ({
+  type: GOT_STUDENTS,
+  students
+});
+
+const gotSingleStudent = (student) => ({
+  type: GOT_SINGLE_STUDENT,
+  student
+});
 
 
 // THUNK CREATORS go here:
+export const fetchStudents = () => async (dispatch) => {
+  const {data} = await axios.get('/api/students');
+  dispatch(gotStudents(data));
+}
+
+export const fetchSingleStudent = (id) => async (dispatch) => {
+  const {data} = await axios.get(`/api/students/${id}`);
+  dispatch(gotSingleStudent(data));
+}
 
 
-const initialState = {};
+const initialState = {
+  students: [],
+  singleStudent: {}
+};
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
+    case GOT_STUDENTS:
+      return {
+        ...state,
+        students: action.students
+      }
+    case GOT_SINGLE_STUDENT:
+      return {
+        ...state,
+        singleStudent: action.student
+      }
     default:
       return state;
   }
 }
 
-const store = createStore(reducer, applyMiddleware(loggerMiddleware));
-store.dispatch({type: 'HELLO_WORLD'})
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
 
 export default store;
